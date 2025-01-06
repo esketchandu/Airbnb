df344ade257ebce133f68a15f07cdee7b1ccec99
esketchandu <esketchandu10@gmail.com>
2025-01-05 16:00:02 -0800

    update a booking route path added

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -13,8 +13,11 @@ const { restoreUser } = require("../../utils/auth.js");
 // Connect restoreUser middleware to the API router
   // If current user session is valid, set req.user to the user in the database
   // If current user session is not valid, set req.user to null
+
+// Restore user session
 router.use(restoreUser);
 
+// Define routes
 router.use('/session', sessionRouter);
 
 router.use('/users', usersRouter);
