d858d9b43ec9fbb019995d063483d7bb33beb08a
esketchandu <esketchandu10@gmail.com>
2025-05-18 02:48:55 -0700

    spots and spotimages seeder files updated

--- backend/db/seeders/03-populate-spotimages.js
+++ backend/db/seeders/03-populate-spotimages.js
@@ -7,39 +7,71 @@ if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;  // define your schema in options object
 }
 
-const { SpotImage } = require('../models'); // Import the SpotImage model
+const { Spot, SpotImage } = require('../models'); // Import the SpotImage model
+
 
 module.exports = {
   async up(queryInterface, Sequelize) {
+    const spots = await Spot.findAll({ order: [['createdAt', 'ASC']] });
     await SpotImage.bulkCreate(
       [
         {
-          spotId: 1,
-          url: 'https://image1.jpg',
+          spotId: spots[0].id,
+          url: 'https://images.pexels.com/photos/1643383/pexels-photo-1643383.jpeg',
           preview: true,
         },
         {
-          spotId: 1,
-          url: 'https://image2.jpg',
+          spotId: spots[0].id,
+          url: 'https://images.pexels.com/photos/2029663/pexels-photo-2029663.jpeg',
           preview: false,
         },
         {
-          spotId: 2,
-          url: 'https://image3.jpg',
+          spotId: spots[1].id,
+          url: 'https://images.pexels.com/photos/2102587/pexels-photo-2102587.jpeg',
           preview: true,
         },
         {
-          spotId: 3,
-          url: 'https://image4.jpg',
+          spotId: spots[2].id,
+          url: 'https://images.pexels.com/photos/4258275/pexels-photo-4258275.jpeg',
           preview: true,
         },
         {
-          spotId: 4,
-          url: 'https://image5.jpg',
+          spotId: spots[3].id,
+          url: 'https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg',
+          preview: true,
+        },
+        {
+          spotId: spots[4].id,
+          url: 'https://images.pexels.com/photos/12700376/pexels-photo-12700376.jpeg',
+          preview: true,
+        },
+        {
+          spotId: spots[5].id,
+          url: 'https://images.pexels.com/photos/5178034/pexels-photo-5178034.jpeg',
+          preview: true,
+        },
+        {
+          spotId: spots[6].id,
+          url: 'https://images.pexels.com/photos/4469137/pexels-photo-4469137.jpeg',
+          preview: true,
+        },
+        {
+          spotId: spots[7].id,
+          url: 'https://images.pexels.com/photos/8583869/pexels-photo-8583869.jpeg',
+          preview: true,
+        },
+        {
+          spotId: spots[8].id,
+          url: 'https://images.pexels.com/photos/8031956/pexels-photo-8031956.jpeg',
+          preview: true,
+        },
+        {
+          spotId: spots[9].id,
+          url: 'https://images.pexels.com/photos/13544219/pexels-photo-13544219.jpeg',
           preview: true,
         },
       ],
-      { validate: true } // Ensures data is validated against the model
+      { validate: true, ...options } // Ensures data is validated against the model
     );
   },
 
@@ -47,7 +79,19 @@ module.exports = {
     options.tableName = 'SpotImages';
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete(options, {
-      url: { [Op.in]: ['https://image1.jpg', 'https://image2.jpg', 'https://image3.jpg', 'https://image4.jpg', 'https://image5.jpg'] }
+      url: { [Op.in]: [
+        'https://images.pexels.com/photos/1643383/pexels-photo-1643383.jpeg',
+        'https://images.pexels.com/photos/2029663/pexels-photo-2029663.jpeg',
+        'https://images.pexels.com/photos/2102587/pexels-photo-2102587.jpeg',
+        'https://images.pexels.com/photos/4258275/pexels-photo-4258275.jpeg',
+        'https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg',
+        'https://images.pexels.com/photos/12700376/pexels-photo-12700376.jpeg',
+        'https://images.pexels.com/photos/5178034/pexels-photo-5178034.jpeg',
+        'https://images.pexels.com/photos/4469137/pexels-photo-4469137.jpeg',
+        'https://images.pexels.com/photos/8583869/pexels-photo-8583869.jpeg',
+        'https://images.pexels.com/photos/8031956/pexels-photo-8031956.jpeg',
+        'https://images.pexels.com/photos/13544219/pexels-photo-13544219.jpeg'
+      ] }
     }, {});
   }
 };
