05f1cecc69ed9d4c47228e44d058208bd9d8dada
esketchandu <esketchandu10@gmail.com>
2025-06-16 19:36:18 -0700

    app jsx updated

--- frontend/src/components/Spots/DeleteSpotModal.jsx
+++ frontend/src/components/Spots/DeleteSpotModal.jsx
@@ -2,7 +2,7 @@ import { useDispatch } from 'react-redux';
 import { deleteSpot } from '../../store/spots';
 import { useModal } from '../../context/Modal';
 import { useNavigate } from 'react-router-dom';
-import './DeleteSpotModal.css'; // we’ll create simple css for this
+import './DeleteSpotModal.css';
 
 function DeleteSpotModal({ spotId }) {
   const dispatch = useDispatch();
@@ -12,13 +12,13 @@ function DeleteSpotModal({ spotId }) {
   const handleDelete = async () => {
     await dispatch(deleteSpot(spotId));
     closeModal();
-    navigate('/manage-spots');
+    navigate('/spots/current');
   };
 
   return (
     <div className="delete-modal">
       <h2>Confirm Delete</h2>
-      <p>Are you sure you want to remove this spot from the listings?</p>
+      <p>Are you sure you want to remove this spot?</p>
 
       <div className="delete-buttons">
         <button className="confirm" onClick={handleDelete}>Yes (Delete Spot)</button>
